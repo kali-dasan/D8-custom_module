dcef650106c7b5ea56d6ac4421ab06e3c5074ab3
kalidasan <kalidasan.seetharaman@capgemini.com>
2016-03-30 19:19:52 +0530

    kali:Drupal8 custom module & custom block

---
+++ first_module/src/Controller/FirstController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\first_module\Controller;
+use Drupal\Core\Controller\ControllerBase;
+
+class FirstController extends ControllerBase {
+	public function content() {
+		return array(
+			'#type' => 'markup',
+			'#markup' => t('Hello world! First module.'),
+		);
+	}
+}
+
+?>
